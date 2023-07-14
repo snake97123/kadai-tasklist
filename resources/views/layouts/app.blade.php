19a4d8f1a61620fecf8a28db85d2768aec524be9
snake97123 <snake12379@yahoo.co.jp>
2023-07-14 02:25:42 +0000

    create CRUD

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>MessageBoard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@2.24.0/dist/full.css" rel="stylesheet" type="text/css" />
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    
+    <body>
+        @include('commons.navbar')
+        
+        <div class="container mx-auto">
+            @include('commons.error_messages')
+            
+            @yield('content')
+        </div>
+    </body>
+</html>
